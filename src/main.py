0b7c860f36078973f2a860dcee764dd5f753c6ae
Gabriel de Lépine -- Ackers <gabriel.delepine.ackers@gmail.com>
2025-01-12 19:18:52 +0100

    :test_tube: Add test for  place_piece

--- src/main.py
+++ src/main.py
@@ -3,3 +3,7 @@ def create_grid(row, col):
     for i in range(row):
         grid.append([0] * col)
     return grid
+
+
+def place_piece(grid, player, row, col):
+    pass
--- src/test.py
+++ src/test.py
@@ -1,4 +1,4 @@
-from main import create_grid
+from main import create_grid, place_piece
 
 
 def test_create_grid():
@@ -8,4 +8,12 @@ def test_create_grid():
     assert all(cell == 0 for row in grid for cell in row)
 
 
+def test_place_piece():
+    grid = create_grid(3, 3)
+    assert place_piece(grid, 1, 0, 1) is True
+    assert grid[0][1] == 1
+    assert place_piece(grid, 2, 0, 1) is False
+
+
 test_create_grid()
+test_place_piece()
